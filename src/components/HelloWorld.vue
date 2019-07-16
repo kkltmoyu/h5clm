a6cdd122c24fd279b6582e8c0caeb8187fae6146
mo.yu <mo.yu@tendcloud.com>
2019-07-16 17:18:07 +0800

    init for fun

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="hello">
+        <mt-range v-model="rangeValue"></mt-range>
+        <div class='row_one'></div>
+        <div class='row_two'></div>
+        <div class='row_three'></div>
+        
+    </div>
+</template>
+<script>
+export default {
+    name: 'HelloWorld',
+    props: {
+        msg: String
+    },
+    data() {
+        return {
+            rangeValue: 30
+        }
+    }
+}
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.hello{
+    display:flex;
+    flex-direction:column;
+
+    .row_one{
+        width:100%;
+        height:100px;
+        background:gray;
+    }
+
+    .row_two{
+        width:100%;
+        height:200px;
+        background:yellow;
+    }
+
+    .row_three{
+        width:100%;
+        height:100px;
+        background:gray;
+    }
+}
+
+</style>
