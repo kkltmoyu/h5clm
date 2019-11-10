d7542272091f4f715860ef3d264d82f1015d0d93
kkltmoyu <kkltmoyu@sina.com>
2019-11-10 19:17:19 +0800

    feat:update

--- src/assets/js/constants.js
+++ src/assets/js/constants.js
@@ -1,4 +1,122 @@
 export const staticUrl = 'assets/img/activity/';
 
+export const categoryList = [{
+    title:'美食',
+    key:'美食',
+    icon:'',
+    children:[{
+        title:'全部',
+        key:'全部',
+        icon:'',
+    },{
+        title:'简餐便当',
+        key:'简餐便当',
+        icon:'jiancanbiandang',
+    },{
+        title:'地方菜系',
+        key:'地方菜系',
+        icon:'jiancanbiandang',
+    },{
+        title:'小吃炸串',
+        key:'小吃炸串',
+        icon:'',
+    },{
+        title:'面食粥点',
+        key:'面食粥点',
+        icon:'',
+    },{
+        title:'日韩料理',
+        key:'日韩料理',
+        icon:'',
+    },{
+        title:'轻食西餐',
+        key:'轻食西餐',
+        icon:'qingshixican',
+    },{
+        title:'汉堡披萨',
+        key:'汉堡披萨',
+        icon:'',
+    },{
+        title:'香锅冒菜',
+        key:'香锅冒菜',
+        icon:'',
+    }]
+},{
+    title:'快捷便当',
+    key:'快捷便当',
+    icon:'',
+    children:[]
+},{
+    title:'特色菜系',
+    key:'特色菜系',
+    icon:'',
+    children:[]
+},{
+    title:'全球美食',
+    key:'全球美食',
+    icon:'',
+    children:[]
+},{
+    title:'小吃夜宵',
+    key:'小吃夜宵',
+    icon:'',
+    children:[]
+},{
+    title:'甜品饮品',
+    key:'甜品饮品',
+    icon:'tianpinyinliao',
+    children:[]
+},{
+    title:'时令水果',
+    key:'时令水果',
+    icon:'shilingshuiguo',
+    children:[]
+},{
+    title:'生鲜买菜',
+    key:'生鲜买菜',
+    icon:'',
+    children:[]
+},{
+    title:'商店超市',
+    key:'商店超市',
+    icon:'',
+    children:[]
+},{
+    title:'鲜花绿植',
+    key:'鲜花绿植',
+    icon:'xianhualvzhi',
+    children:[]
+},{
+    title:'医药健康',
+    key:'医药健康',
+    icon:'',
+    children:[]
+},{
+    title:'早餐',
+    key:'早餐',
+    icon:'',
+    children:[]
+},{
+    title:'午餐',
+    key:'午餐',
+    icon:'',
+    children:[]
+},{
+    title:'下午茶',
+    key:'下午茶',
+    icon:'xiawucha',
+    children:[]
+},{
+    title:'晚餐',
+    key:'晚餐',
+    icon:'',
+    children:[]
+},{
+    title:'夜宵',
+    key:'夜宵',
+    icon:'',
+    children:[]
+}]
+
 
 
